2dc7eba66a6929a879f25b54b2758af51db30976
Clara Schulz-Theißen <Schulz-Theissen@eevolution.de>
2021-04-30 21:27:35 +0200

    Ui changes

--- QEntangle.Wpf/ViewModels/ChoiceEntryViewModel.cs
+++ QEntangle.Wpf/ViewModels/ChoiceEntryViewModel.cs
@@ -1,13 +1,40 @@
-﻿using QEntangle.Wpf.Composite;
+﻿using Prism.Commands;
+using QEntangle.Wpf.Composite;
 using QEntangle.Wpf.Services;
+using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 
 namespace QEntangle.Wpf.ViewModels
 {
   public class ChoiceEntryViewModel : BaseViewModel<ChoiceGetData>
   {
+    #region Constructors
+
+    public ChoiceEntryViewModel()
+    {
+      this.ExecuteChoiceCommand = new DelegateCommand(this.ExecuteChoiceCommandExecute);
+    }
+
+    #endregion Constructors
+
+    #region Properties
+
+    public ICommand ExecuteChoiceCommand { get; set; }
+
     public string Name { get; set; }
 
     public ICollection<string> Options { get; set; }
+
+    #endregion Properties
+
+    #region Methods
+
+    private void ExecuteChoiceCommandExecute()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion Methods
   }
 }
